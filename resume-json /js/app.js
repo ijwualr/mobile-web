b1664c20c97fe7de2c36b4c89cdd152047530c6a
ijwualr <157427990+ijwualr@users.noreply.github.com>
2025-03-14 18:23:55 +0000

    commit

---
+++ resume-json /js/app.js
@@ -0,0 +1,6 @@
+const hamburger = document.getElementById('hamburger-menu');
+const navLinks = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', function() {
+  navLinks.classList.toggle('active');
+});
