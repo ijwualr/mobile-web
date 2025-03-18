2d0383de0ff50dc3e83eb0056e6f7e1059dd4959
ijwualr <157427990+ijwualr@users.noreply.github.com>
2025-03-18 02:28:08 +0000

    java

--- resume-json /js/app.js
+++ resume-json /js/app.js
@@ -1,21 +1,18 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const hamburger = document.querySelector(".hamburger");
-    const nav = document.querySelector("nav");
-    const body = document.querySelector("body"); // Select the body element
-  
-    // Toggle the 'active' class to show or hide the menu
-    hamburger.addEventListener("click", function() {
-      nav.classList.toggle("active");
-      body.classList.toggle("nav-open"); // Add or remove a class to the body
-    });
-  });
-  
-  window.addEventListener('scroll', function () {
+window.addEventListener('scroll', function () {
     const fadeElements = document.querySelectorAll('.fade-in');
     fadeElements.forEach((el) => {
       const rect = el.getBoundingClientRect();
       if (rect.top <= window.innerHeight && rect.bottom >= 0) {
         el.classList.add('scrolled');
       }
+    });
+  });
+  document.addEventListener('DOMContentLoaded', function() {
+    const hamburger = document.querySelector('.hamburger');
+    const nav = document.querySelector('nav');
+
+    hamburger.addEventListener('click', function() {
+      // Toggle the active class on nav
+      nav.classList.toggle('active');
     });
   });
